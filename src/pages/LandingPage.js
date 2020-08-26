f0feaa718148cad043d512786769ac73718e0902
luckmer <woszczo04@gmail.com>
2020-08-26 15:34:23 +0200

    map fix

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,18 +1,39 @@
 import React, { useContext } from "react";
 import { DataContext } from "../data/Data";
 import { Link } from "react-router-dom";
-import { Container, ImgPanel } from "../styles/LandingStyle";
+import {
+    Container,
+    ImgOne,
+    ImgTwo,
+    ImgThree,
+    ImgFour,
+} from "../styles/LandingStyle";
 function LandingPage() {
     const { fetchData } = useContext(DataContext);
+    const Mapping = fetchData.map(({ src }) => src);
+
     return (
         <Container>
-            {fetchData.map(({ src, id }) => (
-                <ImgPanel>
-                    <Link to="/catalog">
-                        <img src={src} alt={src} key={id} />
-                    </Link>
-                </ImgPanel>
-            ))}
+            <ImgOne>
+                <Link to="/details">
+                    <img src={Mapping[0]} />
+                </Link>
+            </ImgOne>
+            <ImgTwo>
+                <Link to="/details">
+                    <img src={Mapping[2]} />
+                </Link>
+            </ImgTwo>
+            <ImgThree>
+                <Link to="/details">
+                    <img src={Mapping[3]} />
+                </Link>
+            </ImgThree>
+            <ImgFour>
+                <Link to="/details">
+                    <img src={Mapping[11]} />
+                </Link>
+            </ImgFour>
         </Container>
     );
 }
--- src/styles/LandingStyle.js
+++ src/styles/LandingStyle.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
     width: 100vw;
     min-height: 100vh;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: repeat(7, 1fr);
     grid-gap: 10px;
     grid-template-rows: auto;
     grid-template-areas:
@@ -13,36 +13,6 @@ export const Container = styled.div`
         " img1 img1  img1 img3 img3 img2 img4"
         " img1 img1 img1  img3 img3 img2 img4"
         ". . . . . . . ";
-`;
-export const ImgPanel = styled.div`
-    &:nth-child(1) {
-        grid-area: img1;
-    }
-    &:nth-child(2) {
-        grid-area: img3;
-    }
-    &:nth-child(3) {
-        grid-area: img2;
-    }
-    &:nth-child(4) {
-        grid-area: img3;
-    }
-    &:nth-child(12) {
-        grid-area: img4;
-    }
-    &:nth-child(13),
-    &:nth-child(5),
-    &:nth-child(6),
-    &:nth-child(7),
-    &:nth-child(8),
-    &:nth-child(9),
-    &:nth-child(10),
-    &:nth-child(11),
-    &:nth-child(14),
-    &:nth-child(15),
-    &:nth-child(16) {
-        display: none;
-    }
     img {
         box-shadow: 11px 11px 52px -14px rgba(0, 0, 0, 0.2);
         border-radius: 5px;
@@ -52,4 +22,16 @@ export const ImgPanel = styled.div`
             transform: scale(1.1);
         }
     }
+`;
+export const ImgOne = styled.div`
+    grid-area: img1;
+`;
+export const ImgTwo = styled.div`
+    grid-area: img2;
+`;
+export const ImgThree = styled.div`
+    grid-area: img3;
+`;
+export const ImgFour = styled.div`
+    grid-area: img4;
 `;
