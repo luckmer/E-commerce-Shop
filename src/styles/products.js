cd19ade9b2018b0bffa1e22bdd2032433ca4f659
luckmer <woszczo04@gmail.com>
2020-08-26 19:22:02 +0200

    few changes

---
+++ src/styles/products.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    min-height: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+        " carousel carousel carousel carousel carousel carousel carousel"
+        " . . . . . . ."
+        " . shoes shoes shoes shoes shoes ."
+        " slider slider slider slider slider slider slider"
+        " contact contact contact contact contact contact contact";
+`;
+
+export const Section = styled.section`
+    grid-area: carousel;
+    .carousel-inner > .item > img {
+        width: 240px;
+        height: 1px;
+    }
+    .carousel-inner {
+        width: 100%;
+        height: 100vh !important;
+    }
+`;
+export const Contact = styled.section`
+    grid-area: contact;
+    color: black;
+    div {
+        width: 100%;
+        height: 30vh;
+        background-color: black;
+    }
+`;
+export const Shoes = styled.section`
+    grid-area: shoes;
+    padding: 10vh 1vw 2vh 1vw;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    flex: 1.36;
+    img {
+        border-radius: 2px;
+        width: 20vw;
+        flex: 2;
+    }
+`;
