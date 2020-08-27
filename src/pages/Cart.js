825c481d3418962f7f2223e7a2191feb87e58801
luckmer <woszczo04@gmail.com>
2020-08-27 14:58:32 +0200

    Cart Control

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,19 +1,63 @@
 import React, { useContext } from "react";
 import { DataContext } from "../data/Data";
+import { Link } from "react-router-dom";
+import {
+    Container,
+    ShopHeader,
+    ScrollPanel,
+    ContextContainer,
+    Box,
+    Row,
+    Section,
+} from "../styles/CartStyles";
 
 function Cart() {
-    const { DataControl } = useContext(DataContext);
+    const { DataControl, Delete } = useContext(DataContext);
 
     return (
-        <div>
-            {DataControl.table.map(({ src, price, _id, context }) => (
-                <div key={_id}>
-                    <img src={src} alt={src} />
-                    <p>{price}</p>
-                    <h5>{context}</h5>
-                </div>
-            ))}
-        </div>
+        <Container>
+            <ShopHeader>
+                <p>Items : {DataControl.table.length}</p>
+                <p>Total : 0$</p>
+            </ShopHeader>
+            <ScrollPanel>
+                {DataControl.table.map(
+                    ({ src, _id, context, title, count, price }) => (
+                        <ContextContainer key={_id}>
+                            <img src={src} alt={src} />
+                            <Box>
+                                <Row>
+                                    <h2> {title}</h2>
+                                    <span> ${price}</span>
+                                </Row>
+                                <p> {context}</p>
+                                <div>
+                                    <button>-</button>
+                                    <span>{count}</span>
+                                    <button>+</button>
+                                </div>
+                            </Box>
+                            <div
+                                style={{
+                                    cursor: "pointer",
+                                    fontWeight: "bold",
+                                }}
+                                onClick={Delete}
+                            >
+                                X
+                            </div>
+                        </ContextContainer>
+                    )
+                )}
+            </ScrollPanel>
+
+            <Section>
+                <Link to="/catalog">
+                    <button>Continue</button>
+                </Link>
+                <button>Buy</button>
+            </Section>
+        </Container>
     );
 }
 export default Cart;
