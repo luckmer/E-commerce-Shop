4e91b1f1b0c407e49aecd9dd2f6801828f2fc9dd
luckmer <woszczo04@gmail.com>
2020-09-22 17:45:08 +0200

    tiny filter Improvement

--- src/components/FilterEffect.js
+++ src/components/FilterEffect.js
@@ -1,12 +1,41 @@
-import { useEffect } from "react";
-function FilterEffect(content, search, setFilteredData) {
+import { useEffect, useContext } from "react";
+import { DataContext } from "../data/Data";
+
+function FilterEffect(search, setFilteredData) {
+    const { DataControl } = useContext(DataContext);
+
     useEffect(() => {
-        const filter = content.filter((state) => {
-            return state.name
-                .toLowerCase()
-                .includes(search.toLocaleLowerCase());
-        });
-        setFilteredData(filter);
-    }, [search, content, setFilteredData]);
+        let filterS = DataControl.fetchData.slice();
+
+        if (search.filterInput) {
+            filterS = filterS.filter((item) =>
+                item.name
+                    .toLowerCase()
+                    .includes(search.filterInput.toLowerCase())
+            );
+        }
+        if (search.filterMark) {
+            filterS = filterS.filter((item) =>
+                item.type
+                    .toLowerCase()
+                    .includes(search.filterMark.toLowerCase())
+            );
+        }
+        if (search.filterPrice) {
+            filterS = filterS.filter((item) =>
+                item.price.toString().includes(search.filterPrice.toString())
+            );
+        }
+        console.log(search);
+
+        setFilteredData(filterS);
+    }, [
+        search.filterInput,
+        DataControl.fetchData,
+        search.filterMark,
+        search.filterPrice,
+        setFilteredData,
+        search,
+    ]);
 }
 export default FilterEffect;
