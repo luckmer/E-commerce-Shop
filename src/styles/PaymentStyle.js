1220f3bc4eb37d61d20049d3da2243a96b5ad0a3
luckmer <woszczo04@gmail.com>
2020-09-28 13:12:06 +0200

    payment css

---
+++ src/styles/PaymentStyle.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ContainerContext = styled.div`
+    padding: 15vh 0 0 0;
+`;
+
+export const FormContext = styled.div`
+    margin: 0 40px 0 40px;
+    button {
+        border-radius: 10px;
+        padding: 10px 20px 10px 20px;
+        background-color: #1d65c1;
+        color: #fff;
+    }
+    form,
+    div {
+        padding: 0;
+        width: 100%;
+    }
+    @media (min-width: 1000px) {
+        button {
+            border-radius: 10px;
+            padding: 1em 5em 1em 5em;
+            background-color: #1d65c1;
+            color: #fff;
+        }
+        margin: 0 20em 0 20em;
+        display: flex;
+    }
+`;
+
+export const ButtonPanel = styled.div`
+    display: flex;
+    justify-content: center;
+`;
