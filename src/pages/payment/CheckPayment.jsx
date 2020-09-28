50f066f9090255967822302733ab9972a5c9dd9d
luckmer <woszczo04@gmail.com>
2020-09-28 15:52:13 +0200

    payment control

--- src/pages/payment/CheckPayment.jsx
+++ src/pages/payment/CheckPayment.jsx
@@ -1,12 +1,44 @@
 import React, { useContext } from "react";
-import { DataContext } from "../../data/Data";
+import { DataContext } from "../../utils/Data";
 import IncorrectPage from "../404";
 import ShippingData from "../../components/Payment/ShippingData";
 import PaymentData from "../../components/Payment/PaymentData";
+import CartData from "../../components/Payment/CartData";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+
+const Container = styled.div`
+    padding: 15vh 10vw 0 10vw;
+
+    div {
+        padding: 0;
+        width: 100%;
+    }
+`;
+const Context = styled.div`
+    display: flex;
+`;
+
+const CompleteButton = styled.div`
+    margin: 30px 0 30px 0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    button {
+        border-radius: 100px;
+        padding: 1em 10em 1em 10em;
+        background-color: #1d65c1;
+        color: #fff;
+    }
+`;
+
 function CheckPayment() {
+    const history = useHistory();
     const {
+        edit,
         store: {
-            BUY: [payment],
+            DATA: [, setTableState],
+            BUY: [payment, setPayment],
         },
     } = useContext(DataContext);
 
@@ -15,11 +47,31 @@ function CheckPayment() {
     if (shipping.length === 0 || paymentCont.length === 0)
         return <IncorrectPage />;
 
+    const Buy = () => {
+        setTableState({ table: [] });
+        setPayment({
+            shipping: [],
+            paymentCont: [],
+        });
+        history.push("/");
+    };
+
     return (
-        <div>
-            <ShippingData data={shipping} />
-            <PaymentData data={paymentCont} />
-        </div>
+        <Container>
+            <Context>
+                <div>
+                    <ShippingData data={shipping} edit={edit} />
+                    <hr />
+                    <PaymentData data={paymentCont} edit={edit} />
+                    <CompleteButton>
+                        <button onClick={Buy}>place an order </button>
+                    </CompleteButton>
+                </div>
+                <div>
+                    <CartData />
+                </div>
+            </Context>
+        </Container>
     );
 }
 
