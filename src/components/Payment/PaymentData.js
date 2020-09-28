d04c6ba6d3e82caa85c03466b579b7600b0c8c8e
luckmer <woszczo04@gmail.com>
2020-09-28 15:52:48 +0200

    payment control

--- src/components/Payment/PaymentData.js
+++ src/components/Payment/PaymentData.js
@@ -1,7 +1,37 @@
 import React from "react";
+import EditPaymentData from "../Edit/EditPaymentData";
 
-function PaymentData({ data }) {
-    return <div></div>;
+function PaymentData({ data, edit }) {
+    console.log(data);
+    return (
+        <div>
+            {data.map(
+                ({
+                    id,
+                    CreditCardNumber,
+                    address,
+                    theTown,
+                    region,
+                    postcode,
+                    NameSurname,
+                    expiryDate,
+                }) => (
+                    <EditPaymentData
+                        key={id}
+                        edit={edit}
+                        CreditCardNumber={CreditCardNumber}
+                        address={address}
+                        theTown={theTown}
+                        region={region}
+                        postcode={postcode}
+                        id={id}
+                        NameSurname={NameSurname}
+                        expiryDate={expiryDate}
+                    />
+                )
+            )}
+        </div>
+    );
 }
 
 export default PaymentData;
--- src/components/Payment/ShippingData.js
+++ src/components/Payment/ShippingData.js
@@ -1,33 +1,52 @@
 import React from "react";
 
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: left;
+    h1,
+    h4,
+    h4 {
+        font-weight: bold;
+    }
+`;
+const Button = styled.button`
+    border-radius: 100px;
+    padding: 5px 50px 5px 50px;
+    background-color: #1d65c1;
+    color: #fff;
+`;
 function ShippingData({ data }) {
     return (
         <div>
-            <div>
-                {data.map(
-                    ({
-                        name,
-                        id,
-                        Surname,
-                        address,
-                        theTown,
-                        PostalCode,
-                        phoneNumber,
-                        EmailAddress,
-                    }) => (
-                        <div key={id}>
-                            <p>{name}</p>
-                            {Surname}
-                            {address}
-                            {theTown}
-                            {PostalCode}
-                            {phoneNumber}
-                            {EmailAddress}
+            {data.map(
+                ({
+                    name,
+                    id,
+                    address,
+                    theTown,
+                    PostalCode,
+                    phoneNumber,
+                    EmailAddress,
+                }) => (
+                    <Container key={id}>
+                        <h1>SENDER</h1>
+                        <h2>{name}</h2>
+                        <div>
+                            <p>address</p>
+                            <h4> {address}</h4>
+                            <h5>{theTown}</h5>
+                            <h5> {PostalCode}</h5>
+                            <h5>phone number : {phoneNumber}</h5>
+                            <h5>email address :{EmailAddress}</h5>
                         </div>
-                    )
-                )}
-            </div>
-            );
+                    </Container>
+                )
+            )}
+            <Button>Edit</Button>
         </div>
     );
 }
