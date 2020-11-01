0e61ca48ce360b1cb4862c27e14c0c5d8436716d
luckmer <woszczo04@gmail.com>
2020-11-01 15:38:00 +0100

    reducers update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
-
-
+import paymentSlice from "./PaymentSlice"
+import contextSlice from "./ContextSlice"
 const Index = combineReducers({
-
+    Context: contextSlice,
+    PaymentContext:paymentSlice,
 })
 export default Index;
