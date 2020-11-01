c2d9113d2ec9b1c202497a9fb370617704db1a67
luckmer <woszczo04@gmail.com>
2020-11-01 15:08:46 +0100

    redux store

---
+++ src/utils/Store.js
@@ -0,0 +1,28 @@
+import { createStore } from 'redux';
+import Index from "../reducers/index";
+
+
+const Save = (state) =>{
+    try {
+        const Save = JSON.stringify(state)
+        localStorage.setItem("CONTEXT",Save)
+    } catch (err) {
+        throw err;
+    }
+}
+
+const Load = () =>{
+    try {
+        const Load = localStorage.getItem("CONTEXT")
+        if ( Load=== null) return undefined
+        return JSON.parse(Load)
+    } catch (err) {
+        throw err;
+    }
+}
+
+const Store = createStore(Index, Load());
+
+Store.subscribe(()=> Save(Store.getState()))
+
+export default Store; 
