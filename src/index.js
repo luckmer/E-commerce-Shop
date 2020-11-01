f88edf8f27191a8472d1d2dd6c5bf32d8a2d4394
luckmer <woszczo04@gmail.com>
2020-11-01 15:09:04 +0100

    index

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import GlobalStyle from "./styles/GlogalStyle";
+
+import Store from "./utils/Store";
+import { Provider } from "react-redux";
+
 ReactDOM.render(
     <Router>
-        <GlobalStyle />
-        <App />
+        <Provider store ={Store}>
+            <GlobalStyle />
+            <App />
+        </Provider>
     </Router>,
     document.getElementById("root")
 );
